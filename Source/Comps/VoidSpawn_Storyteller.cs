ba2cb993d72844e21c70fa36734f414b5281d943
b2952 <b2952@SURGEDRIVERII>
2023-02-13 21:01:01 -0600

    又炸又炸

---
+++ Source/Comps/VoidSpawn_Storyteller.cs
@@ -0,0 +1,16 @@
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using Verse.Sound;
+using UnityEngine;
+using System;
+using System.Linq;
+using HarmonyLib;
+
+namespace InTheDark
+{
+    public class StorytellerComp_CatalystBait: StorytellerComp
+    {
+
+    }
+}
